a089f769e11e2ef81412601bf04cc847f9b3a53c
Sahm Abu Hashish <archi.sahm@gmail.com>
2023-07-04 12:35:16 +0000

    +

--- hashmap_repeated_word/hashmap_repeated_word.py
+++ hashmap_repeated_word/hashmap_repeated_word.py
@@ -1,21 +1,11 @@
 def repeated_word(string):
-    # Remove punctuation and convert the string to lowercase
-    cleaned_string = ''.join(c.lower() if c.isalpha() else ' ' for c in string)
-
-    # Split the cleaned string into a list of words
-    words = cleaned_string.split()
-
-    # Create a dictionary to keep track of word frequencies
     word_counts = {}
-
     # Iterate through the words and check for repetition
-    for word in words:
-        if word in word_counts:
+    for word in string.split():
+        if word.lower() in word_counts:
             # If the word is repeated, return it
             return word
         else:
-            # Update the word count
-            word_counts[word] = 1
-
+            word_counts[word.lower()] = True
     # If no repeated words are found, return None
     return None
